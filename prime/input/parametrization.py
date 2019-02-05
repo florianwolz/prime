Fixed the generation of coefficient and added methods for spatial derivatives of expressions

prime/input/parametrization.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from sympy import Symbol, diff, Function, symbols, O
+from sympy import Symbol, diff, Function, symbols, O, sympify
 import numpy as np
 
 from .field import Field
@@ -32,11 +32,31 @@ def jet_variables(phi, order):
     return ["{}_{}".format(phi, "".join(sorted([chr(ord('x') + e) for e in i]))) for i in idx]
 
 
+def formal_derivative_of_symbol(symbol, direction, independentVariables=['x','y','z']):
+    # Check if the direction checks out
+    if not direction in independentVariables: 
+        raise Exception("'{}' is not a independent variables. Expected one of {}".format(direction, independentVariables))
+
+    # Make sure the derivative is just a string
+    symbolStr = str(symbol)
+
+    # Split the string
+    splitted = symbolStr.split('_')
+
+    # No partial derivative yet at the symbol name, add the direction
+    if len(splitted) == 1: return Symbol("{}_{}".format(symbolStr, direction))
+
+    # Build the multi index
+    multiIndex = sorted([independentVariables.index(i) for i in splitted[1] + [direction]])
+    result = [independentVariables[i] for i in multiIndex]
+    return Symbol("{}_{}".format(splitted[0], "".join(result)))
+
+
 def dPhis(phis, order):
     if order == 0: return np.array(phis)
     from itertools import product
     comps = product(list(range(len(phis))), *[(0,1,2) for i in range(order)])
-    l = ["{}_{}".format(phis[c[0]], "".join(sorted([chr(ord('x') + e) for e in c[1:]]))) for c in comps]
+    l = [Symbol("{}_{}".format(phis[c[0]], "".join(sorted([chr(ord('x') + e) for e in c[1:]])))) for c in comps]
     return np.reshape(l, (len(phis),) + tuple([3 for i in range(order)]))
 
 
@@ -86,7 +106,7 @@ class Parametrization:
         self.dofs = sorted(set([dof for field in self.fields for dof in field.dofs]), key=sortkey_natural)
 
         # Add some methods to generally apply to expressions
-        self._onZero = np.vectorize(lambda x : x.subs([(dof, 0) for dof in self.dofs]) )
+        self._onZero = np.vectorize(lambda x : sympify(x).subs([(dof, 0) for dof in self.dofs]) )
         self._deriv = np.vectorize(lambda x : np.array([diff(x, dof) for dof in self.dofs ]), signature='()->(n)')
 
         # Create a cache for the normal deformation coefficient
@@ -162,3 +182,107 @@ class Parametrization:
         for i in range(order-1):
             res = [x*y for x in res for y in self.dofs]
         return O(sum(res), *self.dofs)
+
+
+"""
+Calculate the spatial derivative of an expression.
+
+If we don't give a direction, the result is a numpy tensor with the derivatives
+in all directions, with the first index being the derivative index.
+
+Args:
+    expr        The expression to derive
+    direction   In which spatial direction should we take the derivative?
+                If none, the result is a vector in all three spatial directions
+"""
+def spatial_diff(expr, direction=None, order=None):
+    if order is not None and type(order) is int:
+        if order == 0:
+            return expr
+        elif order == 1:
+            return spatial_diff(expr=expr, direction=direction)
+        else:
+            return spatial_diff(expr=spatial_diff(expr, direction=direction), direction=direction, order=order-1)
+
+    # No direction? Return the vector x,y,z
+    if direction is None:
+        return np.array([spatial_diff(expr, d) for d in ['x','y','z']])
+
+    # Also allow for lists
+    if type(expr) is list:
+        return np.array([spatial_diff(e, direction) for e in expr])
+
+    # Also applicable for numpy arrays
+    if type(expr) is np.ndarray:
+        tmp = np.vectorize(lambda x : np.array(spatial_diff(x, direction)))
+        return tmp(expr)
+
+    # If there is a big O present, treat it separately
+    if expr.getO() is not None:
+        expr1 = spatial_diff(expr.removeO(), direction)
+        expr2 = spatial_diff(expr.getO().expr, direction)
+        return np.array([a+O(b) for a,b in zip(expr1, expr2)])
+
+    # Get all the symbols
+    syms = [s for s in expr.free_symbols if s not in ['x', 'y', 'z']]
+
+    # Handle explicit x, y, z dependency
+    result = diff(expr, Symbol(dir))
+
+    # Add all the chain rule terms
+    for s in syms:
+        result = result + diff(expr, s) * formal_derivative_of_symbol(s, direction)
+        
+    return np.array(result)
+
+
+class jet_diff(object):
+    def __init__(self, parametrization):
+        if __debug__:
+            if not type(parametrization) is Parametrization:
+                raise Exception("Cannot create a jet derivative helper with a parametrization of wrong type.")
+    
+        # Only need the degrees of freedom
+        self.dofs = parametrization.dofs
+    
+    def __call__(self, expr, direction=None, order=None):
+        if direction is None and order is None:
+            raise Exception("Cannot calculate the jet derivative if neither a direction or an order is given.")
+        
+        # If we are given a derivative order, calculate the tensor into all the direction
+        if direction is None:
+            # Make sure order is an int
+            if type(order) is not int:
+                raise Exception("The order `{}` is not an int.".format(order))
+            
+            # Get all the jet variables at that order
+            vars = dPhis(self.dofs, order)
+            shape = vars.shape
+
+            # Apply the derivative into all directions
+            # TODO: Get rid of duplicate calculations
+            l = np.vectorize(lambda x : self(expr, direction=x, order=None))
+            diffs = np.array(l(vars).tolist())
+
+            # The derivative indices are now the first ones. Move them to the back.
+            return diffs.reshape(tuple(range(len(shape), len(diffs))) + tuple(range(len(shape))))
+
+        # Check if direction is a proper jet variable
+        if __debug__:
+            v = direction.split("_")
+            if v not in self.dofs: raise Exception("Invalid direction.")
+            if len(v) == 2:
+                for d in v[1]:
+                    if d not in ['x', 'y', 'z']:
+                        raise Exception("Invalid direction.")
+
+        # Also allow for lists
+        if type(expr) is list:
+            return np.array([self(e, direction=direction) for e in expr])
+
+        # Also applicable for numpy arrays
+        if type(expr) is np.ndarray:
+            tmp = np.vectorize(lambda x : np.array(self(x, direction=direction)))
+            return tmp(expr)
+        
+        return np.array(diff(expr, direction))

prime/utils.py
@@ -36,8 +36,18 @@ def symmetrize(tensor, indices):
     # Generate all the permutations of the indices
     perms = list(itertools.permutations(indices))
 
-    # Turn the permutations into index shapes
-    shapes = [tuple([perm[i] if i in indices else i for i in range(len(tensor.shape))]) for perm in perms]
+    # Turn them into index shapes
+    shapes = []
+    for perm in perms:
+        shape = []
+        pos = 0
+        for i in range(len(tensor.shape)):
+            if not i in indices:
+                shape.append(i)
+            else:
+                shape.append(perm[pos])
+                pos = pos + 1
+        shapes.append(tuple(shape))
 
     # Add the different permutations of the tensor
     result = np.zeros(tensor.shape)
@@ -163,3 +173,18 @@ def det(matrix):
 def sqrt(expr):
     from sympy import sqrt
     return sqrt(expr)
+
+
+def binomial(N, K):
+    if K == 0: return 1
+    if 2*K > N: K = N-K
+    result = 1
+    for i in range(1, K+1):
+        result = result * (N-K+i)/i
+    return int(result)
+
+
+def factorial(N):
+    if N==0: return 1
+    elif N==1: return 1
+    else: return N*factorial(N-1)